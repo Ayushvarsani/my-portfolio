679b3e4d352407d5be80b7481cf3cfd8a704c843
Ayushvarsani1313 <ayush.v@intacting.in>
2025-08-02 16:26:12 +0530

    modfy the header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,11 +62,12 @@ const Navbar = () => {
             onClick={() => scrollToSection('home')}
             className="flex items-center space-x-2"
           >
+            <img src="/av.svg" alt="Ayush Varsani" width={57} height={30} className='bg-transparent' />
             <motion.div
               whileHover={{ scale: 1.05 }}
               className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent"
             >
-              Portfolio
+              Ayush Varsani
             </motion.div>
           </button>
 
